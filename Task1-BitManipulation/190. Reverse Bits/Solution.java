c6a688cd2822b53da778af52368d42a2165aeb72
Aadarsh P P <151537852+abx1709@users.noreply.github.com>
2024-10-06 14:03:25 +0530

    Update Solution.java

--- Task1-BitManipulation/190. Reverse Bits/Solution.java
+++ Task1-BitManipulation/190. Reverse Bits/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
         for (int i = 0; i < 32; i++) {
             res <<= 1;
             res |= (n & 1);
-            n >>= 1;
+            n >>>= 1;
         }
 
         return res;
