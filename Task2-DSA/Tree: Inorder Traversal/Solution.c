c396bec645d6cdeda14f1e1b1b04c8ce4d2f172c
Aadarsh P P <151537852+abx1709@users.noreply.github.com>
2024-10-06 11:15:07 +0530

    Create Solution.c

---
+++ Task2-DSA/Tree: Inorder Traversal/Solution.c
@@ -0,0 +1,18 @@
+/*
+node is defined as  
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+*/
+
+void inOrder( struct node *root) {
+    if (root==NULL) { return; }
+    inOrder(root->left);
+    printf("%d ", root->data);
+    inOrder(root->right);
+}
