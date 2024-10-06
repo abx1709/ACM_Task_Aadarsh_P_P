1e8f3e8582994f7f06eaeeaa806b88786e743b09
Aadarsh P P <151537852+abx1709@users.noreply.github.com>
2024-10-06 11:06:12 +0530

    Create Solution.java

---
+++ Task1-BitManipulation/67. Add Binary/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int i = a.length()-1;
+        int j = b.length()-1;
+
+        StringBuilder res = new StringBuilder();
+        int carry = 0;
+        while (i>-1 || j>-1 || carry>0) {
+            int val1 = (i > -1) ? a.charAt(i) - '0' : 0; i--;
+            int val2 = (j > -1) ? b.charAt(j) - '0' : 0; j--;
+
+            int s = val1 + val2 + carry;
+            carry = (s & 2) >> 1;
+            res.append(s & 1);
+        }
+
+        return res.reverse().toString();
+    }
+}
